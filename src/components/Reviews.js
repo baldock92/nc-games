ec1ac952e87b3868e9976123295f8d3fbea477b3
baldock92 <george_baldock@hotmail.co.uk>
2022-06-14 13:03:53 +0100

    add categories page with links to respective reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import {useParams} from "react-router-dom"
 import { getReviews } from "../utils/api";
 import "../styles/reviews.css";
 
@@ -6,14 +7,18 @@ const Reviews = () => {
   const [allReviews, setAllReviews] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
+  const { category } = useParams();
 
-    getReviews().then((reviews) => {
-      setAllReviews(reviews);
-      setLoading(false);
-    });
-  }, []);
+  useEffect(() => {
+      setLoading(true);
+
+      getReviews(category).then((reviews) => {
+        setAllReviews(reviews);
+        setLoading(false);
+      });
+    },
+    [category]
+  );
 
   if (loading) {
     return <div>Loading...</div>;
@@ -21,18 +26,20 @@ const Reviews = () => {
 
   return (
     <div className="reviews__all">
-        {allReviews.map((review) => {
-            return (
-                <div className="review__card" key={review.review_id}>
-                <img className="review__image"  src={review.review_img_url} alt="board game" />
-                <div className="review__title"> {review.title}</div>
-                <div className="review__category">Category: {review.category}</div>
-                <div className="review__id">Review ID: {review.review_id}</div>
-
-                </div>
-            )
-        })}
-      
+      {allReviews.map((review) => {
+        return (
+          <div className="review__card" key={review.review_id}>
+            <img
+              className="review__image"
+              src={review.review_img_url}
+              alt="board game"
+            />
+            <div className="review__title"> {review.title}</div>
+            <div className="review__category">Category: {review.category}</div>
+            <div className="review__id">Review ID: {review.review_id}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
