84610cf91f34cf05c1dc9d0af6428e1d4b54678b
baldock92 <george_baldock@hotmail.co.uk>
2022-06-15 12:02:50 +0100

    single review functionality, add links from all reviews to single review

---
+++ src/components/SingleReview.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getReviewById } from "../utils/api";
+import "../styles/singleReview.css";
+
+const SingleReview = () => {
+  const [review, setReview] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const { review_id } = useParams();
+
+  useEffect(() => {
+      setLoading(true);
+      
+
+      getReviewById(review_id).then((reviewFromApi) => {
+        setReview(reviewFromApi);
+        setLoading(false);
+      });
+    },
+    [review_id]
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="singleReview__card">
+      <h3 className="singleReview__title">{review.title}</h3>
+      <h5 className="singleReview__category">A {review.category} game</h5>
+      <img src={review.review_img_url} className="singleReview__image" alt="board game" />
+      <ul className="singleReview__author">Review by {review.owner}</ul>
+      <li className="singleReview__body">{review.review_body}</li>
+      <li className="singleReview__createdAt">Review written: {review.created_at}</li>
+      <li className="singleReview__votes">Number of votes : {review.votes}</li>
+      <li className="singleReview__comments">Number of comments: {review.comment_count}</li>
+    </div>
+  );
+};
+
+export default SingleReview;
--- src/utils/api.js
+++ src/utils/api.js
@@ -5,13 +5,29 @@ const gamesApi = axios.create({
 });
 
 export const getReviews = (category) => {
-  return gamesApi.get(`/reviews`, { params: { category } }).then(({ data }) => {
-    return data.reviews;
-  });
+  return gamesApi
+    .get(`/reviews`, { params: { category } })
+    .then(({ data }) => {
+      return data.reviews;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const getCategories = () => {
   return gamesApi.get("/categories").then(({ data }) => {
     return data.categories;
   });
+};
+
+export const getReviewById = (review_id) => {
+  return gamesApi
+    .get(`/reviews/${review_id}`)
+    .then(({ data }) => {
+      return data.review;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
