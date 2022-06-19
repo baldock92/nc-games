60532157c5c894fe691a4febd3803eba48e2fabf
baldock92 <george_baldock@hotmail.co.uk>
2022-06-19 14:53:25 +0100

    add basic home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,17 @@
+import "../styles/Home.css"
+
 const Home = () => {
   return (
     <>
-      <h2>Home page</h2>
+      <h2 className="Home__header">Home page</h2>
       <br />
-      <h3>Don't forget to add some stuff here later you fool</h3>
+      <h3 className="Home__title">Welcome to my board games review app!</h3>
       <br />
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/G1IbRujko-A"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <h4>You are logged in as <span className="Home__user">grumpy19</span></h4>
+      <img className="Home__img"
+        src="https://i.guim.co.uk/img/media/696d305e8fb4ec8e33038325f5e135ad7e6f62b5/0_878_3586_2149/master/3586.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=7c8a02cb30d1bc9b008a31529aa98fc1"
+        alt="monopoly board game"
+      />
     </>
   );
 };
