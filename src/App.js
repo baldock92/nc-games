cf69090ec532495338a6759bfcaded1db081c7f8
baldock92 <george_baldock@hotmail.co.uk>
2022-09-05 12:31:58 +0100

    user context added, login and logut functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles/App.css";
-
+import { useState } from "react";
 import Header from "./components/Header";
 // import Login from "./components/Login"
 import Navbar from "./components/Navbar";
@@ -7,11 +7,16 @@ import Reviews from "./components/Reviews";
 import Categories from "./components/Categories";
 import Home from "./components/Home";
 import SingleReview from "./components/SingleReview";
-import ErrorPage from "./components/ErrorPage"
+import ErrorPage from "./components/ErrorPage";
 import { Routes, Route } from "react-router-dom";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+  const [user, setUser] = useState("")
+
+
   return (
+    <UserContext.Provider value={{user, setUser}}>
     <div className="App">
       <div className="banner-top">
         <Header />
@@ -27,6 +32,7 @@ function App() {
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </div>
+    </UserContext.Provider>
   );
 }
 
